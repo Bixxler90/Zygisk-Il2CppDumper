09652e3d131e41330637132820741fe076b63135
Bixxler90 <66971706+Bixxler90@users.noreply.github.com>
2025-02-20 10:24:03 +0100

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -46,33 +46,37 @@ private:
     size_t length;
 
     void preSpecialize(const char *package_name, const char *app_data_dir) {
-        if (strcmp(package_name, GamePackageName) == 0) {
-            LOGI("detect game: %s", package_name);
-            enable_hack = true;
-            game_data_dir = new char[strlen(app_data_dir) + 1];
-            strcpy(game_data_dir, app_data_dir);
 
-#if defined(__i386__)
-            auto path = "zygisk/armeabi-v7a.so";
-#endif
-#if defined(__x86_64__)
-            auto path = "zygisk/arm64-v8a.so";
-#endif
-#if defined(__i386__) || defined(__x86_64__)
-            int dirfd = api->getModuleDir();
-            int fd = openat(dirfd, path, O_RDONLY);
-            if (fd != -1) {
-                struct stat sb{};
-                fstat(fd, &sb);
-                length = sb.st_size;
-                data = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
-                close(fd);
+        for(auto GamePackageName : games_list){
+   
+            if (strcmp(package_name, GamePackageName) == 0) {
+                LOGI("detect game: %s", package_name);
+                enable_hack = true;
+                game_data_dir = new char[strlen(app_data_dir) + 1];
+                strcpy(game_data_dir, app_data_dir);
+    
+    #if defined(__i386__)
+                auto path = "zygisk/armeabi-v7a.so";
+    #endif
+    #if defined(__x86_64__)
+                auto path = "zygisk/arm64-v8a.so";
+    #endif
+    #if defined(__i386__) || defined(__x86_64__)
+                int dirfd = api->getModuleDir();
+                int fd = openat(dirfd, path, O_RDONLY);
+                if (fd != -1) {
+                    struct stat sb{};
+                    fstat(fd, &sb);
+                    length = sb.st_size;
+                    data = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
+                    close(fd);
+                } else {
+                    LOGW("Unable to open arm file");
+                }
+    #endif
             } else {
-                LOGW("Unable to open arm file");
+                api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
             }
-#endif
-        } else {
-            api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
         }
     }
 };
